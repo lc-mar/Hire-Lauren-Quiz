4deebb64fa3bad2502262bfe9d83369cdefb8482
Lauren <78833615+lc-mar@users.noreply.github.com>
2021-10-12 06:43:32 -0400

    updte script

--- script.js
+++ script.js
@@ -1,18 +1,111 @@
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
-startButton.addEventListener('click',startGame)+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
 function startGame() {
-console.log('Started')
-startButton.classList.add('hide')
-questionContainerElement.classList.remove('hide')
-setNextQuestion()
-}
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+  }
 
 function setNextQuestion() {
-
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function selectAnswer() {
-
+function showQuestion(question) {
+    questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
 }
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+      answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+  }
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+      element.classList.add('correct')
+    } else {
+      element.classList.add('wrong')
+    }
+  }
+
+  function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+  }
+  
+const questions = [
+    {
+        question: 'Are you interested in hiring Lauren Marcinek?',
+        answers: [
+          { text: 'Yes', correct: true },
+          { text: 'No', correct: false }
+        ]
+      },
+      {
+        question: 'Who is the most qualified candidate for this role?',
+        answers: [
+          { text: 'Seth Rogen', correct: false },
+          { text: 'Pablo Picasso', correct: false },
+          { text: 'Lauren Marcinek', correct: true },
+          { text: 'Harry Potter', correct: false }
+        ]
+      },
+      {
+        question: 'Will you regret hiring Lauren Marcinek?',
+        answers: [
+          { text: 'Who could say?', correct: false },
+          { text: 'Yes', correct: false },
+          { text: "I would never regret the best decision I've ever made.", correct: true },
+          { text: 'What?', correct: false }
+        ]
+      },
+      {
+        question: 'Who loves making creative and fun code?',
+        answers: [
+          { text: 'Anyone Else', correct: false },
+          { text: 'Lauren', correct: true }
+        ]
+      }
+    ]
